7edc5cbd82f5915c267387f77693a645a07ecb92
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-07-28 19:20:54 +0530

    Spring MicroServices

--- PhotoAppApiUsers/src/main/java/com/photoappapi/users/rest/controller/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/photoappapi/users/rest/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-	@GetMapping("status/check")
+	@GetMapping("/status/check")
 	public String status(){
 		return "Working";
 	}

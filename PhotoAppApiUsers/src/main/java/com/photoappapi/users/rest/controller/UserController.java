237f674522e5e0bd1402c706fdb84d2c36735efe
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-08-10 15:52:56 +0530

    Spring MicroServices

--- PhotoAppApiUsers/src/main/java/com/photoappapi/users/rest/controller/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/photoappapi/users/rest/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.photoappapi.users.rest.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
+
+	private Environment env;
 
 	@GetMapping("/status/check")
 	public String status(){
-		return "Working";
+		return "Working on port " + env.getProperty("local.server.port");
 	}
 }

43ef7af5351df8a023efa542428b1d64df1b6cb9
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-08-17 19:41:46 +0530

    Spring MicroServices Section - 12

--- PhotoAppApiUsers/src/main/java/com/photoappapi/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/photoappapi/users/security/WebSecurity.java
@@ -19,5 +19,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+		http.headers().frameOptions().disable();
 	}
 }

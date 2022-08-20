205425af5b4c42b7c58a64fa80395ba5fed3f111
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-08-20 18:21:00 +0530

    Spring MicroServices Section - 17

--- PhotoApiConfigServer/src/main/java/com/example/photoapp/api/photoapiconfigserver/PhotoApiConfigServerApplication.java
+++ PhotoApiConfigServer/src/main/java/com/example/photoapp/api/photoapiconfigserver/PhotoApiConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.example.photoapp.api.photoapiconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PhotoApiConfigServerApplication {
 
 	public static void main(String[] args) {

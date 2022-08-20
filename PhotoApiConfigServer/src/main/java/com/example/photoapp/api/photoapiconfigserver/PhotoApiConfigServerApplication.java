e0d59762dc4741e6748ebcda89e899bac4e2b055
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-08-20 18:14:18 +0530

    Spring MicroServices Section - 17

---
+++ PhotoApiConfigServer/src/main/java/com/example/photoapp/api/photoapiconfigserver/PhotoApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoapp.api.photoapiconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoApiConfigServerApplication.class, args);
+	}
+
+}
---
+++ PhotoApiConfigServer/src/test/java/com/example/photoapp/api/photoapiconfigserver/PhotoApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoapp.api.photoapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

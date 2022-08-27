e9bdd75dd06c8a0f9c4583bf4d2a0f5b6968d7ae
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-08-27 18:02:33 +0530

    Spring MicroServices Section - 25,26

--- PhotoAppApiUsers/src/main/java/com/photoappapi/users/service/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/photoappapi/users/service/AlbumsServiceClient.java
@@ -1,10 +1,12 @@
 package com.photoappapi.users.service;
 
 import com.photoappapi.users.rest.dtos.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,5 +18,12 @@ import java.util.List;
 public interface AlbumsServiceClient {
 
 	@GetMapping("/users/{id}/albums")
+	@CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
 	List<AlbumResponseModel> getAlbums(@PathVariable String id);
+
+	default List<AlbumResponseModel> getAlbumsFallback(String id, Throwable exception){
+		System.out.println("Param = " + id);
+		System.out.println("Exception took place: " + exception.getMessage());
+		return new ArrayList<>();
+	}
 }
